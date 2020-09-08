ecde729e52e2293cbd9f770d7c16f35868943a43
Franz <fk.luepke146@uniandes.edu.co>
2020-09-08 08:18:03 -0500

    Changes

--- src/poses_publish.py
+++ src/poses_publish.py
@@ -18,73 +18,24 @@ class Poses(object):
 		
 	def path_msg(self):
 
-		path = Path()
-		path.header = Header()
-		path.header.frame_id = 'Test'
+		msg = Path()
+		msg.header.frame_id = "/map"
+		msg.header.stamp = rospy.Time.now()
+
 		poseStamped = PoseStamped()
-		poseStamped.header = Header()
-		poseStamped.header.frame_id = 'Test'
-		
-		ini = Pose()
-		ini.position = Point(0.0,0.0,0.0)
-		end = Pose()
-		end.position = Point(0.0,0.0,0.0)
-		
-		poseStamped = [ini,end]
-		path.poses = poseStamped
-		print(path)
-		end.position.x = self.end_pose[0]
-		end.position.y = self.end_pose[1]
-		end.position.z = self.end_pose[2]
-		# pose.pose = ini
-		pose = [ini,end]
-		path.poses = pose
-		#print(path)
-		# path.poses.append(pose)
-		# pose.pose = end
-		# pose.pose = end
-		# path.poses.append(end)
-		# path_pub.publish(<path)
-		# path = Path()
-		# ini = Pose()
-		# ini.position.x = 2
-		# ini.position.y = 3
-		# ini.position.z = 4
-		# # path.append(ini)
-		# # path.header = Header()
-		# # path.poses.header = Header()
-		# path.poses.append(ini)
-		# # path.poses[0].pose.position.x = 2
-		# print(path)
-		return path
+		poseStamped.pose.position.x = self.ini_pose[0]
+		poseStamped.pose.position.y = self.ini_pose[1]
+		poseStamped.pose.position.z = self.ini_pose[2]
 
+		poseStamped.pose.orientation.x = 0.0
+		poseStamped.pose.orientation.y = 0.0
+		poseStamped.pose.orientation.z = 0.0
+		poseStamped.pose.orientation.w = 0.0
+		
+		msg.poses.append(poseStamped)
+		print(msg)
+		return msg
 
-		# # Init pose
-		# ini_pose_msg = Pose()
-		# ini_pose_msg.position = self.ini_pose
-		# # End Pose
-		# end_pose_msg = Pose()
-		# end_pose_msg.position = self.end_pose
-		
-		# header = Header()
-		# path = Path()
-		# path.header = Header()		
-		# path.poses = [ini_pose_msg,end_pose_msg]
-		
-		# # pose.header = data.header
-		# # pose.pose = data.pose.pose
-		# # path.poses.append(pose
-		# # return msg
-		# path = Path()
-		# path.header = Header()
-		# path.poses = []
-		# # poseStamped = PoseStamped()
-		# # poseStamped.header = Header()
-		# # poseStamped.pose = pose.pose.pose
-		# # path.poses.append(self.ini_pose)
-		# # path.poses.append(self.end_pose)
-		# # path_pub.publish(path)
-		# return path
 
 def main():
 	try:
