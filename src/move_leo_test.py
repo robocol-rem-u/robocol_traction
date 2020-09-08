9a42e8c0f86b402ff98e4e3dd2d589adf12f1b1b
Eloyabmfcv <ea.briceno@uniandes.edu.co>
2020-09-08 17:19:40 -0500

    inicio union con vision

--- src/move_leo_test.py
+++ src/move_leo_test.py
@@ -10,45 +10,49 @@ from geometry_msgs.msg import Twist, TwistStamped,PoseWithCovariance,Pose
 #from leo_control_auto.msg import Obstacle
 from std_msgs.msg import Int32
 
+
 class MoveLeo(object):
 	def __init__(self):
 		super(MoveLeo, self).__init__()
 		self.posicionActual = 0.0
-		self.hayRuta = True
+		self.twistDirection = 0.0
+		self.pausar=False
+		self.hayRuta = False
+		self.detectStation = False
+		self.stationCentered = False
 		self.ruta = []
-		self.ruta = [[18.03,-9.26],[20.03,-9.26],[20.03,-7.26],[18.03,-7.26],[18.03,-9.26]]
-
-		#vec = [[-6.57433, -8.74819],[-6, -7], [-4.537165, -5.874095], [-2.5, -3], [0.87335, 1.320051667], [4.2467, 4.92031], [9.72025, 2.5208465], [15.1938, 0.121383]]
-		#for i in vec:
-		#	self.ruta.append([i[0]-vec[0][0],i[1]-vec[0][1]])
-		#print(vec)
-
+		rospy.Subscriber('/robocol/ruta', Float32MultiArray, self.setRutaCallback)
 		self.x,self.y,self.theta = 0.0,0.0,0.0
 		print('Init node...')
 		rospy.init_node('leo_move', anonymous=True)
-		rospy.Subscriber('/controllers/diff_drive/odom',Odometry, self.setPositionCallback)
-		rospy.Subscriber('/robocol/IMU_euler',Twist, self.callback_IMU)
-		rospy.Subscriber('/robocol/ruta',Float32MultiArray, self.callback_ruta)
+		rospy.Subscriber('/sim_gazebo_pose', Odometry, self.setPositionCallback)
+		rospy.Subscriber('/robocol/pause', bool, self.setPausar)
 		self.pubVel = rospy.Publisher('/cmd_vel',Twist, queue_size=10)
 
 	def callback_IMU(self,param):
 		self.theta = param.angular.z
 		# print(param)
 
-	def callback_ruta(self,param):
-		print(param)
+	def setRutaCallback(self,pruta):
+		self.ruta=pruta.data
+		self.hayRuta=True
 
 	def girar(self,param):
-		kp = 0.2
-		ka = 0.4 + 0.6 * np.exp(-param)
+		kp = 0.4
+		ka = 0.5 + 0.3 * np.exp(-param)
 		giro = Twist()
 		w = ka * param + kp * np.sin(param) * np.cos(param)
 		giro.angular.z = w
+		# while (self.pausar is True):
+		# 	msg = Twist()
+		# 	msg.linear.x = 0.0
+		# 	msg.angular.z = 0.0
+		# 	self.pubVel.publish(msg)
 		self.pubVel.publish(giro)
 
 	def adelantar(self,rho, alpha):
-		kp = 0.4 #+ 0.6 * np.exp(-rho)
-		ka = 0.5 #+ 0.5 * np.exp(-alpha)
+		kp = 1 + 0.5 * np.exp(-rho)
+		ka = 1.5 + 0.5 * np.exp(-alpha)
 
 		vmax = 1.5
 		msg = Twist()
@@ -58,7 +62,28 @@ class MoveLeo(object):
 
 		msg.linear.x = v
 		msg.angular.z = w
+		# while (self.pausar is True):
+		# 	msg = Twist()
+		# 	msg.linear.x = 0.0
+		# 	msg.angular.z = 0.0
+		# 	self.pubVel.publish(msg)
+			
 		self.pubVel.publish(msg)
+
+	def pointStation(self):
+		msg = Twist()
+		v = 0
+		if not self.stationCentered:
+			w = 0.3*self.twistDirection
+		else:
+			w = 0
+			calculatePosition()
+		msg.linear.x = v
+		msg.angular.z = w
+		self.pubVel.publish(msg)
+
+	def calculatePosition(self):
+		#Completar
 
 	def setPositionCallback(self,odom):
 		pose = odom.pose.pose
@@ -70,6 +95,13 @@ class MoveLeo(object):
 		roll,pitch,self.theta = self.quat_2_euler(qx,qy,qz,qw)
 		# print(' x: ',pose.orientation.x,' y: ',pose.orientation.y,'z: ',pose.orientation.z,'w: ',pose.orientation.w)
 		# print(' roll: ',roll,' pitch: ',pitch,'yaw: ',yaw)
+
+	def setDetectStationCallback(self, param):
+		#Completar
+		pass
+
+	def setPausar(self,pause):
+		self.pausar=pause
 
 	def quat_2_euler(self,x,y,z,w):
 		t0 =  2.0 * (w * x + y * z)
@@ -117,25 +149,35 @@ class MoveLeo(object):
 							alpha = c-b
 						else:
 							alpha = angulo - self.theta
-						self.girar(alpha)
-						print('a: ',alpha,'t: ',self.theta,'ang: ',angulo)
 
+						while (self.pausar is True):
+							msg = Twist()
+							msg.linear.x = 0.0
+							msg.angular.z = 0.0
+							self.pubVel.publish(msg)
+							
+						# while (self.detectStation is True):
+						# 	pointStation()
+						self.girar(alpha)
+						print('Girando --- rho: {} alpha: {}\r'.format(round(rho,3),round(alpha,3)), end="")
+						# print('a: ',alpha,'t: ',self.theta,'ang: ',angulo)
 
 					msg = Twist()
 					msg.linear.x = 0.0
 					msg.angular.z = 0.0
+					
+	
+						
 					self.pubVel.publish(msg)
 					time.sleep(1)
 
 					print('------------------------------')
-					error = [xf - self.x, yf - self.y]
-					angulo = np.arctan2(error[1], error[0])
-					while rho > 0.07 and not rospy.is_shutdown():
+
+					while rho > 0.04 and not rospy.is_shutdown():
 						# print('advance')
 
 						error = [xf - self.x, yf - self.y]
-						print('Error: ',error)
-						# angulo = np.arctan2(error[1], error[0])
+						angulo = np.arctan2(error[1], error[0])
 						rho = np.sqrt(np.power(error[0], 2) + np.power(error[1], 2))
 						if angulo > 2.8 or angulo < -2.8:
 							b = np.pi - abs(angulo)
@@ -145,11 +187,19 @@ class MoveLeo(object):
 							alpha = c-b
 						else:
 							alpha = angulo - self.theta
-						print(' rho:',rho,' angle: ', angulo,' theta: ', self.theta,' alpha: ', alpha)
-						if alpha > 0.15 or alpha < -0.15:
-							self.girar(alpha)
-						else:
-							self.adelantar(rho, alpha)
+
+						while (self.pausar is True):
+							msg = Twist()
+							msg.linear.x = 0.0
+							msg.angular.z = 0.0
+							self.pubVel.publish(msg)
+
+						while (self.detectStation is True):
+							pointStation()
+
+						# print(' rho:',rho,' angle: ', angulo,' theta: ', self.theta,' alpha: ', alpha)
+						print('Avanzando --- rho: {} alpha: {}\r'.format(round(rho,3),round(alpha,3)), end="")
+						self.adelantar(rho, alpha)
 						# print('a: ',alpha,'t: ',self.theta,'ang: ',angulo)
 			msg = Twist()
 			msg.linear.x = 0.0
