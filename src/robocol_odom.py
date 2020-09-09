27202f9c316f7c4e139ff43986362d68ea9d31c3
Franz <fk.luepke146@uniandes.edu.co>
2020-09-08 20:14:46 -0500

    Odometry changes

--- src/robocol_odom.py
+++ src/robocol_odom.py
@@ -1,32 +1,160 @@
 #!/usr/bin/env python3
 import rospy
-import sys
+import sys,math,time
+import threading
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Twist
 
 class Odom(object):
 	def __init__(self):
 		super(Odom, self).__init__()
-		rospy.loginfo('Starting odometry Robocol node...')
+		self.opciones = True
+		# Local coordinates
+		# self.loc_x, self.loc_y, self.loc_z  = 0.0, 0.0, 0.0
+		# self.loc_rx,self.loc_ry,self.loc_rz = 0.0, 0.0, 0.0
+		# Global coordinates
+		self.glo_x, self.glo_y, self.glo_z  = 0.0, 0.0, 0.0
+		self.glo_rx,self.glo_ry,self.glo_rz = 0.0, 0.0, 0.0
+		# Our global coordinates
+		self.our_glo_x, self.our_glo_y, self.our_glo_z  = 0.0, 0.0, 0.0
+		self.our_glo_rx,self.our_glo_ry,self.our_glo_rz = 0.0, 0.0, 0.0
+		# Global coordinates correction
+		self.corr_glo_x, self.corr_glo_y, self.corr_glo_z  = 0.0, 0.0, 0.0
+		self.corr_glo_rx,self.corr_glo_ry,self.corr_glo_rz = 0.0, 0.0, 0.0
+		# Node init
+		print('Starting robocol_odom node...')
 		rospy.init_node('robocol_odom', anonymous=True)
-		rospy.Subscriber('/sim_gazebo_pose', Odometry, self.odometry_callback)
-		self.pubVel = rospy.Publisher('/robocol/odometry',Odometry, queue_size=1)
+		# Publishers
+		print('Publishing in /robocol/odom (Odometry)\n')
+		self.pubOdom = rospy.Publisher('/robocol/odom',Odometry, queue_size=1)
+		# Subscribers
+		print('Subscribing to /zed2/imu/data (Imu)')
+		rospy.Subscriber('/zed2/imu/data', Imu, self.imu_callback)
+		print('Subscribing to /zed2/odom (Odometry)')
+		rospy.Subscriber('/zed2/odom', Odometry, self.odometry_callback)
+		print('Subscribing to /robocol/vision_correction (Twist)')
+		rospy.Subscriber('/robocol/vision_correction', Twist, self.vision_correction_callback)
+
+		time.sleep(3)
+		rospy.on_shutdown(self.kill)
+		print('')
+
+		x = threading.Thread(target=self.thread_function)
+		x.start()
+
+	def kill(self):
+		print("\nCerrando nodo...")
+		self.opciones = False
+		print('Presione Enter para finalizar...')
+
+	def vision_correction_callback(self,param):
+		print('\nCorreción de visión -> x: {} y: {}\n'.format(param.linear.x,param.linear.y))
+		self.corr_glo_x  = self.corr_glo_x  + ( param.linear.x-self.our_glo_x)
+		self.corr_glo_y  = self.corr_glo_y  + ( param.linear.y-self.our_glo_y)
+		self.corr_glo_z  = self.corr_glo_z  + ( param.linear.z-self.our_glo_z)
+		self.corr_glo_rx = self.corr_glo_rx + (param.angular.x-self.our_glo_rx)
+		self.corr_glo_ry = self.corr_glo_ry + (param.angular.y-self.our_glo_ry)
+		self.corr_glo_rz = self.corr_glo_rz + (param.angular.z-self.our_glo_rz)
 
 	def odometry_callback(self,param):
-		x = round(param.pose.pose.position.x,2)
-		y = round(param.pose.pose.position.y,2)
-		z = round(param.pose.pose.position.z,2)
-		# print('GLOBAL COORDINATES:')
-		# print('\r x: {}, y: {}, z: {}\r'.format(x,y,z), end="\r")
-		sys.stdout.write('GLOBAL COORDINATES: \r')
-		print('')
-		sys.stdout.write(str(x) + "% \r")
-		# cat('GLOBAL COORDINATES: %s','efe')
+		self.glo_x = round(param.pose.pose.position.x,3)
+		self.glo_y = round(param.pose.pose.position.y,3)
+		self.glo_z = round(param.pose.pose.position.z,3)
+		qx = param.pose.pose.orientation.x
+		qy = param.pose.pose.orientation.y
+		qz = param.pose.pose.orientation.z
+		qw = param.pose.pose.orientation.w
+		self.glo_rx,self.glo_ry,self.glo_rz = self.quat_2_euler(qx,qy,qz,qw)
+
+		g_coord = 'GLOBAL x {:.3f} y {:.3f} z {:.3f} rx {:.3f} ry {:.3f} rz {:.3f}'
+		# l_coord = ' LOCAL x {:.3f} y {:.3f} z {:.3f} rx {:.3f} ry {:.3f} rz {:.3f}'
+		msg = '	'+g_coord+'\r'
+		print(msg.format(self.our_glo_x,self.our_glo_y,self.our_glo_z,self.our_glo_rx,self.our_glo_ry,self.our_glo_rz), end="")
+
+		self.our_glo_x  = self.corr_glo_x  + self.glo_x
+		self.our_glo_y  = self.corr_glo_y  + self.glo_y
+		self.our_glo_z  = self.corr_glo_z  + self.glo_z
+		self.our_glo_rx = self.corr_glo_rx + self.glo_rx
+		self.our_glo_ry = self.corr_glo_ry + self.glo_ry
+		self.our_glo_rz = self.corr_glo_rz + self.glo_rz
+
+		odom_msg = Odometry()
+		odom_msg.pose.pose.position.x    = self.our_glo_x
+		odom_msg.pose.pose.position.y    = self.our_glo_y
+		odom_msg.pose.pose.position.z    = self.our_glo_z
+		odom_msg.pose.pose.orientation.x = self.our_glo_rx
+		odom_msg.pose.pose.orientation.y = self.our_glo_ry
+		odom_msg.pose.pose.orientation.z = self.our_glo_rz
+		self.pubOdom.publish(odom_msg)
+
+	def imu_callback(self,param):
+		# Orientation
+		x = param.orientation.x
+		y = param.orientation.y
+		z = param.orientation.z
+		w = param.orientation.w
+		self.imu_roll,self.imu_pitch,self.imu_yaw = self.quat_2_euler(x,y,z,w)
+		# Angular Velocity
+		self.imu_ang_vel_x = param.angular_velocity.x
+		self.imu_ang_vel_y = param.angular_velocity.y
+		self.imu_ang_vel_z = param.angular_velocity.z
+		# Linear Velocity
+		self.imu_lin_vel_x = param.linear_acceleration.x
+		self.imu_lin_vel_y = param.linear_acceleration.y
+		self.imu_lin_vel_z = param.linear_acceleration.z
+
+	def reset_global_coord(self):
+		self.corr_glo_x  = -self.glo_x
+		self.corr_glo_y  = -self.glo_y
+		self.corr_glo_z  = -self.glo_z
+		self.corr_glo_rx = -self.glo_rx
+		self.corr_glo_ry = -self.glo_ry
+		self.corr_glo_rz = -self.glo_rz
+
+	def thread_function(self):
+		while self.opciones:
+			print("Elija una opción:")
+			print(" R: Para resetear coordenadas globales.")
+			# print(" - Para ingresar coordenadas globales (G)")
+			op = input('>')
+			if op == "R":
+				print(" Reseteando coordenadas locales...")
+				self.reset_global_coord()
+			# elif op == "G":
+			# 	print(" Cambiando coordenadas globales...")
+			# 	print("  Ingrese coordenada en x:")
+			# 	x = input('>')
+			# 	print("  Ingrese coordenada en y:")
+			# 	y = input('>')
+			else:
+				print(' COMANDO NO RECONOCIDO.')
+			print('')
+		print('Closing thread...')
+
+	def quat_2_euler(self,x,y,z,w):
+		# Transform quaternions to Euler (roll,pitch,yaw)
+		t0 =  2.0 * (w * x + y * z)
+		t1 =  1.0 - 2.0 * (x * x + y * y)
+		roll = round(math.atan2(t0, t1),3)
+		
+		t2 =  2.0 * (w * y - z * x)
+		t2 =  1.0 if t2 > +1.0 else t2
+		t2 = -1.0 if t2 < -1.0 else t2
+		pitch = round(math.asin(t2),3)
+		
+		t3 =  2.0 * (w * z + x * y)
+		t4 =  1.0 - 2.0 * (y * y + z * z)
+		yaw = round(math.atan2(t3, t4),3)
+
+		return [roll, pitch, yaw]
 
 def main():
 	try:
 		odom = Odom()
 		rate = rospy.Rate(10)
 		while not rospy.is_shutdown():
+			# print('')
 			rate.sleep()
 	except rospy.ROSInterruptException:
 		return
--- src/sim_pose.py
+++ src/sim_pose.py
@@ -1,49 +1,149 @@
 #!/usr/bin/env python3
-import rospy
+import rospy,time,math
 import numpy as np
-import time
-from std_msgs.msg import String, Int32, Float32MultiArray
-from geometry_msgs.msg import Twist
-import math, roslaunch, time
 from gazebo_msgs.msg import LinkStates
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist, TwistStamped,PoseWithCovariance,Pose
-#from leo_control_auto.msg import Obstacle
-from std_msgs.msg import Int32
+from geometry_msgs.msg import Twist,TwistStamped
 
-
-class Pose(object):
+# Sim pose
+class Sim_Pose(object):
 	def __init__(self):
-		super(Pose, self).__init__()
-		print('Init node...')
-		rospy.init_node('sim_gazebo_pose', anonymous=True)
+		super(Sim_Pose, self).__init__()
+		# Wheel odometry
+		self.wheel_x, self.wheel_y, self.wheel_z  = 0.0,0.0,0.0
+		self.wheel_rx,self.wheel_ry,self.wheel_rz = 0.0,0.0,0.0
+		# Gazebo "odometry"
+		self.gaz_x, self.gaz_y, self.gaz_z  = 0.0,0.0,0.0
+		self.gaz_rx,self.gaz_ry,self.gaz_rz = 0.0,0.0,0.0
+		# Robocol odometry
+		self.robocol_x, self.robocol_y, self.robocol_z  = 0.0,0.0,0.0
+		self.robocol_rx,self.robocol_ry,self.robocol_rz = 0.0,0.0,0.0
+		# Node init
+		self.node_is_up = True
+		print('Starting robocol_sim_gazebo_pose node...')
+		rospy.init_node('robocol_sim_gazebo_pose')
+		# Publishers
+		print('Publishing in /zed2/odom (Odometry)')
+		self.pub_odom = rospy.Publisher('/zed2/odom', Odometry, queue_size=1)
+		print('Publishing in /robocol/odom_error (Twist)')
+		self.pub_error = rospy.Publisher('/robocol/odom_error', Twist, queue_size=1)
+		# Subscribers
+		print('Subscribing to /gazebo/link_states (LinkStates)')
 		rospy.Subscriber('/gazebo/link_states', LinkStates, self.poseCallback)
-		self.pub = rospy.Publisher('/sim_gazebo_pose', Odometry, queue_size=1)
+		print('Subscribing to /wheel_odom (LinkStates)')
+		rospy.Subscriber('/wheel_odom', TwistStamped, self.wheel_odom_callback)
+		print('Subscribing to /robocol/odom (Odometry)')
+		rospy.Subscriber('/robocol/odom', Odometry, self.robocol_odom_callback)
+		# On rospy shutdown
+		rospy.on_shutdown(self.kill_node)
 
 	def poseCallback(self, param):
-		msj = Odometry()
-		msj.pose.pose.position.x = param.pose[1].position.x
-		msj.pose.pose.position.y = param.pose[1].position.y
-		msj.pose.pose.position.z = param.pose[1].position.z
+		link_num = param.name.index('leo::base_link')
+		# Publish odometry based on Gazebo link states.
+		odom_msg = Odometry()
+		odom_msg.pose.pose.position.x = param.pose[link_num].position.x
+		odom_msg.pose.pose.position.y = param.pose[link_num].position.y
+		odom_msg.pose.pose.position.z = param.pose[link_num].position.z
+		odom_msg.pose.pose.orientation.y = param.pose[link_num].orientation.y
+		odom_msg.pose.pose.orientation.z = param.pose[link_num].orientation.z
+		odom_msg.pose.pose.orientation.x = param.pose[link_num].orientation.x
+		odom_msg.pose.pose.orientation.w = param.pose[link_num].orientation.w
+		# Mix odoms
+		odom_msg = self.mix_gazebo_n_wheel_odom(odom_msg)
+		self.pub_odom.publish(odom_msg)
+		# Print rounded information on screen.
+		link = param.name[link_num]
+		self.gaz_x = round(odom_msg.pose.pose.position.x,3)
+		self.gaz_y = round(odom_msg.pose.pose.position.y,3)
+		self.gaz_z = round(odom_msg.pose.pose.position.z,3)
+		qx = round(odom_msg.pose.pose.orientation.x,3)
+		qy = round(odom_msg.pose.pose.orientation.y,3)
+		qz = round(odom_msg.pose.pose.orientation.z,3)
+		qw = round(odom_msg.pose.pose.orientation.w,3)
+		self.gaz_rx,self.gaz_ry,self.gaz_rz = self.quat_2_euler(qx,qy,qz,qw)
+		if self.node_is_up:
+			print('  Odometry msg: link: {}   x: {} y: {} z: {}   qx: {} qy: {} qz:: {} qw: {}'.format(link,self.gaz_x,self.gaz_y,self.gaz_z,qx,qy,qz,qw), end="\r")
+	
+	def mix_gazebo_n_wheel_odom(self,odom_msg):
+		# Mix Gazebo with wheels
+		qx,qy,qz,qw = self.euler_2_quat(self.wheel_rx,self.wheel_ry,self.wheel_rz)
+		a = 1
+		odom_msg_2 = Odometry()
+		odom_msg_2.pose.pose.position.x    = a*odom_msg.pose.pose.position.x    + (1-a)*self.wheel_x 
+		odom_msg_2.pose.pose.position.y    = a*odom_msg.pose.pose.position.y    + (1-a)*self.wheel_y 
+		odom_msg_2.pose.pose.position.z    = a*odom_msg.pose.pose.position.z    + (1-a)*self.wheel_z
+		odom_msg_2.pose.pose.orientation.x = a*odom_msg.pose.pose.orientation.x + (1-a)*qx
+		odom_msg_2.pose.pose.orientation.y = a*odom_msg.pose.pose.orientation.y + (1-a)*qy
+		odom_msg_2.pose.pose.orientation.z = a*odom_msg.pose.pose.orientation.z + (1-a)*qz
+		odom_msg_2.pose.pose.orientation.w = a*odom_msg.pose.pose.orientation.w + (1-a)*qw
+		return odom_msg_2
 
-		msj.pose.pose.orientation.x = param.pose[1].orientation.x
-		msj.pose.pose.orientation.y = param.pose[1].orientation.y
-		msj.pose.pose.orientation.z = param.pose[1].orientation.z
-		msj.pose.pose.orientation.w = param.pose[1].orientation.w
+	def wheel_odom_callback(self,param):
+		# Read the wheels odometry
+		self.wheel_frame_id = param.header.frame_id
+		self.wheel_x = param.twist.linear.x
+		self.wheel_y = param.twist.linear.y
+		self.wheel_z = param.twist.linear.z
+		self.wheel_rx = param.twist.angular.x
+		self.wheel_ry = param.twist.angular.y
+		self.wheel_rz = param.twist.angular.z
 
-		print('x: {}, y: {}\r'.format(round(msj.pose.pose.position.x,3),round(msj.pose.pose.position.y,3)), end="")
+	def robocol_odom_callback(self,param):
+		# Robocol odometry
+		self.robocol_x  = param.pose.pose.position.x
+		self.robocol_y  = param.pose.pose.position.y
+		self.robocol_z  = param.pose.pose.position.z
+		self.robocol_rx = param.pose.pose.orientation.x
+		self.robocol_ry = param.pose.pose.orientation.y
+		self.robocol_rz = param.pose.pose.orientation.z
 
-		self.pub.publish(msj)
-		#print(param.pose[1])
-		#print('')
+	def publish_odometry_error(self):
+		# Publish error of odometry
+		error_msg = Twist()
+		error_msg.linear.x  = self.wheel_x  - self.robocol_x
+		error_msg.linear.y  = self.wheel_y  - self.robocol_y
+		error_msg.linear.z  = self.wheel_z  - self.robocol_z
+		error_msg.angular.x = self.wheel_rx - self.robocol_rx
+		error_msg.angular.y = self.wheel_ry - self.robocol_ry
+		error_msg.angular.z = self.wheel_rz - self.robocol_rz
+		self.pub_error.publish(error_msg)
 
+	def quat_2_euler(self,x,y,z,w):
+		# Transform quaternions to Euler (roll,pitch,yaw)
+		t0 =  2.0 * (w * x + y * z)
+		t1 =  1.0 - 2.0 * (x * x + y * y)
+		roll = round(math.atan2(t0, t1),3)
+		
+		t2 =  2.0 * (w * y - z * x)
+		t2 =  1.0 if t2 > +1.0 else t2
+		t2 = -1.0 if t2 < -1.0 else t2
+		pitch = round(math.asin(t2),3)
+		
+		t3 =  2.0 * (w * z + x * y)
+		t4 =  1.0 - 2.0 * (y * y + z * z)
+		yaw = round(math.atan2(t3, t4),3)
 
+		return [roll, pitch, yaw]
+
+	def euler_2_quat(self,roll,pitch,yaw):
+		# Transform Euler (roll,pitch,yaw) to quaternions
+		qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+		qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+		qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+		qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+		return [qx, qy, qz, qw]
+
+	def kill_node(self):
+		# Action taken when node is killed.
+		self.node_is_up = False
+		print("\nKilling node...\n")
 
 def main():
 	try:
-		pose = Pose()
-		rate = rospy.Rate(10)
-		while not rospy.is_shutdown():			
+		sim_pose = Sim_Pose()
+		rate = rospy.Rate(2)
+		while not rospy.is_shutdown():
+			sim_pose.publish_odometry_error()			
 			rate.sleep()
 	except rospy.ROSInterruptException:
 		return
