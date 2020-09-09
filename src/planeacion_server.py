fa880bdab383efad35a201fd8724061e5490d474
Franz <fk.luepke146@uniandes.edu.co>
2020-09-09 09:35:28 -0500

    ROS numpy array

--- src/planeacion_server.py
+++ src/planeacion_server.py
@@ -6,12 +6,16 @@ import numpy as np
 import rospy
 import cv2
 import matplotlib.pyplot as plt
-from robocol_traction.srv import Navegacion, NavegacionResponse, GridmapPoints
+# from robocol_traction.srv import Navegacion, NavegacionResponse, GridmapPoints
 from nav_msgs.msg import Path
 from PIL import Image, ImageDraw
 from std_msgs.msg import Float32MultiArray
 from rospy.numpy_msg import numpy_msg
 from rospy_tutorials.msg import Floats
+import numpy
+from std_msgs.msg import Float32MultiArray
+roslib.load_manifest('rospy')
+from std_msgs.msg import MultiArrayDimension
 
 """
 metodos usados en la clase minheap 
@@ -480,6 +484,11 @@ class Ruta:
 
         response = NavegacionResponse()
         response.rutax, response.rutay = self.gridmap_to_vrep(w, h)
+
+        a=numpy.array(numpy.random.randn(2,3), dtype=numpy.float32) #please ensure the dtype in identifical to the topic type
+        print "sending\n", a
+        pub.publish(data=a)
+        
         print('aqui')
         print(response)
    
@@ -502,6 +511,44 @@ class Ruta:
 
         #return response
 
+    def _serialize_numpy(self, buff):
+        """
+        wrapper for factory-generated class that passes numpy module into serialize
+        """
+        # pass in numpy module reference to prevent import in auto-generated code
+        if self.layout.dim == []:
+            self.layout.dim = [ MultiArrayDimension('dim%d' %i, self.data.shape[i], self.data.shape[i]*self.data.dtype.itemsize) for i in range(len(self.data.shape))];
+        self.data = self.data.reshape([1, -1])[0];
+        return self.serialize_numpy(buff, numpy)
+
+    def _deserialize_numpy(self, str):
+        """
+        wrapper for factory-generated class that passes numpy module into deserialize    
+        """
+        # pass in numpy module reference to prevent import in auto-generated code
+        self.deserialize_numpy(str, numpy)
+        dims=map(lambda x:x.size, self.layout.dim)
+        self.data = self.data.reshape(dims)
+        return self
+
+    ## Use this function to generate message instances using numpy array
+    ## types for numerical arrays. 
+    ## @msg_type Message class: call this functioning on the message type that you pass
+    ## into a Publisher or Subscriber call. 
+    ## @returns Message class
+    def numpy_nd_msg(self,msg_type):
+        classdict = { '__slots__': msg_type.__slots__, '_slot_types': msg_type._slot_types,
+                      '_md5sum': msg_type._md5sum, '_type': msg_type._type,
+                      '_has_header': msg_type._has_header, '_full_text': msg_type._full_text,
+                      'serialize': _serialize_numpy, 'deserialize': _deserialize_numpy,
+                      'serialize_numpy': msg_type.serialize_numpy,
+                      'deserialize_numpy': msg_type.deserialize_numpy
+                      }
+
+        # create the numpy message type
+        msg_type_name = "Numpy_%s"%msg_type._type.replace('/', '__')
+        return type(msg_type_name,(msg_type,),classdict)
+
     def callbackPath(self, param):
         #print('aaaa')
         self.points = param.data
@@ -514,6 +561,7 @@ def main():
     rospy.init_node('navegacion', anonymous=True)
     ruta = Ruta()
     rospy.Subscriber('/robocol/inicio_destino', Float32MultiArray, ruta.callbackPath)
+    pub = rospy.Publisher('mytopic', numpy_nd_msg(Float32MultiArray))
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
